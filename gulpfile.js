771247aba5fb8e253e2a97b239d67e660c5b7779
EnzoDarling <crasshoverride07@gmail.com>
2017-08-08 15:50:38 -0300

    Practicando FrontEnd

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+const imagemin = require('gulp-imagemin');
+//const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+//const concat = require('gulp-concat');
+gulp.task('message', function() {
+  return console.log('Gulp esta iniciando...');
+});
+gulp.task('views', function() {
+  gulp.src('./src/views/*.pug')
+  .pipe(pug({
+    pretty:true/* PARA COMPRIMIR HTML ELIMINAR PRETTY{}*/
+  }))
+  .pipe(gulp.dest('./dist/'))
+});
+gulp.task('sass', function() {
+  gulp.src('./src/sass/*.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css'))
+});
+gulp.task('imageMin', function(){
+  gulp.src('./src/img/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/img'))
+});
+gulp.task('watch', function() {
+  gulp.watch('./src/views/*.pug', ['views']);
+  gulp.watch('./src/sass/*.sass', ['sass']);
+  gulp.watch('./src/img/*', ['imageMin']);
+});
+gulp.task('default', ['message', 'views', 'sass','imageMin', 'watch']);
