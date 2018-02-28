18569df914e0b0e6d20bc642abfe498c68d92def
EnzoDarling <crasshoverride07@gmail.com>
2018-02-28 16:19:49 -0300

    Fixing forms and inputs

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require('gulp');
+const browser = require('browser-sync').create();
 const pug = require('gulp-pug');
 const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
@@ -7,17 +8,27 @@ const sass = require('gulp-sass');
 gulp.task('message', function() {
   return console.log('Gulp esta iniciando...');
 });
+//Server Estatico
+gulp.task('browserSync', () =>{
+	browser.init({
+		server:{
+			baseDir: "./dist"
+		}
+	});
+});
 gulp.task('views', function() {
   gulp.src('./src/views/*.pug')
   .pipe(pug({
     pretty:true/* PARA COMPRIMIR HTML ELIMINAR PRETTY{}*/
   }))
   .pipe(gulp.dest('./dist/'))
+  .pipe(browser.reload({stream: true}));
 });
 gulp.task('sass', function() {
-  gulp.src('./src/sass/*.sass')
+  gulp.src('./src/sass/*')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./dist/css'))
+    .pipe(browser.reload({stream: true}));
 });
 gulp.task('imageMin', function(){
   gulp.src('./src/img/*')
@@ -27,12 +38,13 @@ gulp.task('imageMin', function(){
 gulp.task('minify', function () {
   gulp.src('src/js/*.js')
     .pipe(uglify())
-    .pipe(gulp.dest('dist/js'));
+    .pipe(gulp.dest('dist/js'))
+    .pipe(browser.reload({stream: true}));
 });
 gulp.task('watch', function() {
   gulp.watch('./src/views/*.pug', ['views']);
-  gulp.watch('./src/sass/*.sass', ['sass']);
+  gulp.watch('./src/sass/*', ['sass']);
   gulp.watch('./src/img/*', ['imageMin']);
   gulp.watch('./src/js/*.js', ['minify']);
 });
-gulp.task('default', ['message', 'views', 'sass','imageMin','minify','watch']);
+gulp.task('default', ['message', 'views', 'sass','imageMin','minify','watch','browserSync']);
